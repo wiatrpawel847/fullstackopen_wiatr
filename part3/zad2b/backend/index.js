359ed5c99b1f8d9cba6cd164f54fd00a56643611
=wiatrpawel847 <wiatrpawel847@gmail.com>
2025-05-29 08:50:40 +0200

    server

--- part3/zad2b/backend/index.js
+++ part3/zad2b/backend/index.js
@@ -78,7 +78,7 @@ app.put('/api/persons/:id', (req, res) => {
   res.json(updatedPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
