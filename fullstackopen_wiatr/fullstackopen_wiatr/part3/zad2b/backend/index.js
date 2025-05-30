a683a97df6f414f1a0547d43d1448ac5bf772d8f
=wiatrpawel847 <wiatrpawel847@gmail.com>
2025-05-30 08:53:19 +0200

    213

--- fullstackopen_wiatr/fullstackopen_wiatr/part3/zad2b/backend/index.js
+++ fullstackopen_wiatr/fullstackopen_wiatr/part3/zad2b/backend/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('dist'));
+
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
   { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
@@ -79,4 +81,4 @@ app.put('/api/persons/:id', (req, res) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+});a
